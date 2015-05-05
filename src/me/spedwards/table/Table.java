cd13f99cfbb7b23793f298cb3c0a667824f8737a
Spedwards <improbablepuzzle@gmail.com>
2015-05-05 23:00:32 +1000

    Version 1.0.0
    remove Row

--- src/me/spedwards/table/Table.java
+++ src/me/spedwards/table/Table.java
@@ -7,37 +7,50 @@ import me.spedwards.table.errors.*;
 /**
  * Table object.
  * @author	Liam Edwards
- * @version	1.0.0
+ * @version	1.1.0
  */
 public class Table {
 	
-	private Row header;
-	private ArrayList<Row> rows = new ArrayList<Row>();
+	private ArrayList<String> header;
+	private ArrayList<ArrayList<String>> rows;
 	
 	/**
 	 * Initialize this Table with header row.
-	 * @param	header	row object to be used as the table header.
-	 * @see		Row
+	 * @param	header	ArrayList<String> of cells to be used as the table header.
 	 */
-	public Table(Row header) {
+	public Table(ArrayList<String> header) {
 		this.header = header;
+		this.rows = new ArrayList<ArrayList<String>>();
 	}
 	
 	/**
 	 * Adds a row to this Table.
-	 * @param	row		row object to be added.
+	 * @param	row						ArrayList<String> of cells to be added.
 	 * @throws	TooFewCellsException	If a row has fewer cells than the header.
 	 * @throws	TooManyCellsException	If a row has more cells than the header.
-	 * @see		me.spedwards.table.Row
 	 */
-	public void addRow(Row row) throws TooFewCellsException,TooManyCellsException {
-		if (row.getCells().size() < this.header.getCells().size()) {
+	public void addRow(ArrayList<String> row) throws TooFewCellsException,TooManyCellsException {
+		if (row.size() < this.header.size()) {
 			throw new TooFewCellsException("");
-		} else if (row.getCells().size() > this.header.getCells().size()) {
+		} else if (row.size() > this.header.size()) {
 			throw new TooManyCellsException("");
 		} else {
 			this.rows.add(row);
 		}
+	}
+	
+	/**
+	 * Create a row
+	 * @param	cells	an indefinite amount of strings to be used as cells.
+	 * @return	an ArrayList<String> of cells.
+	 * @since	1.1.0
+	 */
+	public static ArrayList<String> newRow(String... cells) {
+		ArrayList<String> out = new ArrayList<String>();
+		for (String cell : cells) {
+			out.add(cell);
+		}
+		return out;
 	}
 	
 	/**
@@ -53,22 +66,22 @@ public class Table {
 			body = "",
 			line = "+";
 		
-		for (int i = 0; i < header.getCells().size(); i++) {
-			for (int j = 0; j < header.getCells().get(i).length() + 4; j++) {
+		for (int i = 0; i < this.header.size(); i++) {
+			for (int j = 0; j < this.header.get(i).length() + 4; j++) {
 				line += "-";
 			}
 			line += "+";
 		}
-		for (int i = 0; i < header.getCells().size(); i++) {
-			tmp += "|  " + header.getCells().get(i) + "  ";
+		for (int i = 0; i < this.header.size(); i++) {
+			tmp += "|  " + this.header.get(i) + "  ";
 		}
 		tmp += "|";
 		headers = line + "\n" + tmp + "\n" + line;
 		
 		tmp = "";
 		
-		for (Row row : rows) {
-			for (String cell : row.getCells()) {
+		for (ArrayList<String> row : this.rows) {
+			for (String cell : row) {
 				body += "|  " + cell + "  ";
 			}
 			body += "|\n";
@@ -81,17 +94,17 @@ public class Table {
 	
 	private void adjust() {
 		for (int i = 0; i < rows.size(); i++) {
-			for (int j = 0; j < rows.get(i).getCells().size(); j++) {
-				if (rows.get(i).getCells().get(j).length() < header.getCells().get(j).length()) {
-					rows.get(i).getCells().set(j, adjustCell(rows.get(i).getCells().get(j),header.getCells().get(j).length()));
-				} else if (rows.get(i).getCells().get(j).length() > header.getCells().get(j).length()) {
-					header.getCells().set(j, adjustCell(header.getCells().get(j), rows.get(i).getCells().get(j).length()));
+			for (int j = 0; j < rows.get(i).size(); j++) {
+				if (rows.get(i).get(j).length() < header.get(j).length()) {
+					rows.get(i).set(j, adjustCell(rows.get(i).get(j),header.get(j).length()));
+				} else if (rows.get(i).get(j).length() > header.get(j).length()) {
+					header.set(j, adjustCell(header.get(j), rows.get(i).get(j).length()));
 				}
 				for (int l = 0; l < i; l++) {
-					if (rows.get(l).getCells().get(j).length() > rows.get(i).getCells().get(j).length()) {
-						rows.get(i).getCells().set(j, adjustCell(rows.get(i).getCells().get(j),rows.get(l).getCells().get(j).length()));
-					} else if (rows.get(l).getCells().get(j).length() < rows.get(i).getCells().get(j).length()) {
-						rows.get(l).getCells().set(j, adjustCell(rows.get(l).getCells().get(j),rows.get(i).getCells().get(j).length()));
+					if (rows.get(l).get(j).length() > rows.get(i).get(j).length()) {
+						rows.get(i).set(j, adjustCell(rows.get(i).get(j),rows.get(l).get(j).length()));
+					} else if (rows.get(l).get(j).length() < rows.get(i).get(j).length()) {
+						rows.get(l).set(j, adjustCell(rows.get(l).get(j),rows.get(i).get(j).length()));
 					}
 				}
 			}
